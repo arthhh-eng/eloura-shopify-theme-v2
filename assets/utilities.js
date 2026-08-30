853b563c138807510ba2d972877bfbc8467060f2
arthhh-eng <arthhh@icloud.com>
2026-08-30 23:38:07 +0300

    Replace header links with premium navigation drawer

--- assets/utilities.js
+++ assets/utilities.js
@@ -790,7 +790,8 @@ export function setHeaderMenuStyle() {
     window.requestAnimationFrame(() => {
       const overflowList = headerComponent?.querySelector('overflow-list');
       const hasReachedMinimum = overflowList && overflowList.hasAttribute('minimum-reached');
-      headerComponent.dataset.menuStyle = isTouchDevice() || hasReachedMinimum ? 'drawer' : 'menu';
+      const forceDrawer = headerComponent.hasAttribute('data-force-drawer');
+      headerComponent.dataset.menuStyle = forceDrawer || isTouchDevice() || hasReachedMinimum ? 'drawer' : 'menu';
     });
   }
 }
